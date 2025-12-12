49f34767804cc302036c1b2072d817b04617d79e
Bowen Sun <bowensun@gmail.com>
2025-12-12 22:07:39 +0000

    chore: update pidof release

--- Formula/gopidof.rb
+++ Formula/gopidof.rb
@@ -1,33 +1,33 @@
 class Gopidof < Formula
   desc "Lightweight pidof implementation written in Go"
   homepage "https://github.com/nusnewob/gopidof"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nusnewob/gopidof/releases/download/v0.2.0/pidof-darwin-arm64.tar.gz"
-      sha256 "ff00785a95d88f9212b86ebe1c69d655a219284358dca106995c017960a742a6"
+      url "https://github.com/nusnewob/gopidof/releases/download/v0.2.1/pidof-darwin-arm64.tar.gz"
+      sha256 "32d0d030e6b6af0be96bd5d13f8e019edf75e644586eb5f0a126877e0eddfdee"
     else
-      url "https://github.com/nusnewob/gopidof/releases/download/v0.2.0/pidof-darwin-amd64.tar.gz"
-      sha256 "28440504652a2051fbb5e6358369059efd7da5e5ef0dc238b3a2250c6a8e9ee7"
+      url "https://github.com/nusnewob/gopidof/releases/download/v0.2.1/pidof-darwin-amd64.tar.gz"
+      sha256 "9e79c2ac24acaa931cd263d515fcd7e4c9f24063355233b1a49b54cd69d46095"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/nusnewob/gopidof/releases/download/v0.2.0/pidof-linux-arm64.tar.gz"
-      sha256 "a526f17cfdc0229bddb4ee6618a2424a994263b4286ebfbff3c2fabb264be19b"
+      url "https://github.com/nusnewob/gopidof/releases/download/v0.2.1/pidof-linux-arm64.tar.gz"
+      sha256 "a9996d2838e4c702e97f661a52c2615c1341e63b108aa6c794cd1b045faaf846"
     else
-      url "https://github.com/nusnewob/gopidof/releases/download/v0.2.0/pidof-linux-amd64.tar.gz"
-      sha256 "97ac135d06e2a8436157173af5687d5746da0ff1cafefd5c042601f057a7c854"
+      url "https://github.com/nusnewob/gopidof/releases/download/v0.2.1/pidof-linux-amd64.tar.gz"
+      sha256 "ffcafdb4f4e06b1eba8fb4542b1df2333d190c3a046366476713fa7f8eff6abb"
     end
   end
 
   # Fallback to build from source if binary not available
   resource "source" do
-    url "https://github.com/nusnewob/gopidof/releases/download/v0.2.0/gopidof-v0.2.0-source.tar.gz"
-    sha256 "2904f025b144528bedb767ed355fb2d74bae8506ed392e8f09715496cc0ee52e"
+    url "https://github.com/nusnewob/gopidof/releases/download/v0.2.1/gopidof-v0.2.1-source.tar.gz"
+    sha256 "d85f39e3022b21f40430209faba743e3a9732e11ebb726c9c9358ba63eb01ff8"
   end
 
   depends_on "go" => [:build, :optional]
